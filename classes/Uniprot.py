3dce396ab63a7e3e95239aaf587a829d619b3244
Kovarik, Dennis <dennis.kovarik@mines.sdsmt.edu>
2021-06-26 21:18:10 -0600

    Implemented function 'field_supported()' to check if a Uniprot search field is supported or not

--- classes/Uniprot.py
+++ classes/Uniprot.py
@@ -3,6 +3,33 @@ class Uniprot():
     Class for searching the Uniprot Database for EC Numbers for proteins.
     """
     @staticmethod
+    def field_supported(field):
+        """
+        Determines if a Uniprot search field is supported by this software or 
+        by Uniprot itself.
+        
+        :param field: The Uniprot search field.
+        :return: True if the search field is supported
+        :return: False otherwise
+        :return: An error message as a String
+        """
+        # Check the type of field
+        if type(field) != type("string"):
+            err = "Error in field_supported. Type of 'field' parameter must "
+            err += "be a string."
+            return False, err           
+        # Check for field 'All'
+        if field.lower() == "all":
+            return True, "Field Supported"
+            # Check for field 'All'
+        if field.lower() == "protein name":
+            return True, "Field Supported"
+        if field.lower() == "organism":
+            return True, "Field Supported"
+        return False, "Field Not Supported"
+    
+    
+    @staticmethod
     def ec_search(search_terms):
         """
         Performs a search on Uniprot via REST Api give search terms.
@@ -33,4 +60,8 @@ class Uniprot():
                     err = "Error in Uniprot class member function 'ec_search()'. "
                     err += "Elements in tuples must me strings"
                     raise TypeError(err)
+            # Check if search field is supported
+            supported, err = Uniprot.field_supported(e[0])
+            if not supported:
+                raise Exception(error)
                 
