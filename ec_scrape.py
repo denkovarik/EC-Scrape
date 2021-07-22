0c8fc5879b4c2c3a28f68f23cb93a6658881d5d1
Kovarik, Dennis <dennis.kovarik@mines.sdsmt.edu>
2021-07-21 20:31:38 -0600

    Refactored to make code more modular

--- ec_scrape.py
+++ ec_scrape.py
@@ -1,13 +1,13 @@
 # File: ec_scrape.py
 #
 # Description:
-# Performs a blast on sequences in a genome annotation and then searches 
+# Performs a blast on sequences or parses downloaded BLAST result files. 
+# for proteins in a genome annotation. Afterwards it then searches 
 # online databases like NCBI protein and UniProt for their EC Number. This
 # is the main program file which is the start of the program.
 #
 # Author: Dennis Kovarik
 
-import unittest
 import os, io, sys, inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
@@ -18,11 +18,12 @@ from classes.Annot_Reader import *
 from utils import *
 
 
-args = parse_args_ec_scrape()
+args = parse_args_ec_scrape(sys.argv)
 # Read the excel sheet
 reader = Annot_Reader(args)
 if '--from_downloaded_blast' in args.keys() \
 and args['--from_downloaded_blast'] == True:
-    dl_blast_ec_scrape(reader, args)
+    ec_added = dl_blast_ec_scrape(reader, args)
+    print("Found EC Numbers for an additional " + str(ec_added) + " proteins")
 else:
     online_blast_ec_scrape(reader, args)
