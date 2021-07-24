158ee6ef7a747cb17d8dff9fd163ada9baf052cb
Kovarik, Dennis <dennis.kovarik@mines.sdsmt.edu>
2021-07-24 14:24:31 -0600

    Removed obsolete test

--- testing/utils_testing.py
+++ testing/utils_testing.py
@@ -11,27 +11,7 @@ import math
 class Utils_tests(unittest.TestCase):
     """
     Runs all tests for the utility functions in utils.py.
-    """
-    def test_parse_fasta(self):
-        """
-        Tests the ability of the parse_fasta() function to parse a fasta file 
-        containing multiple sequences and store the sequences by their location.
-        
-        :param self: An instance of the Utils_tests class.
-        """
-        filepath = 'E:\\blast\\mult_query.fasta'
-        self.assertTrue(os.path.isfile(filepath))
-        queries = parse_fasta(filepath)
-        exp = {
-            'contig_1_7184_6765': 'MDIKIHSDFSHANLNEMREVYSSVGWTKHTTKIIKQVFEASNVIALATINGRIIGFGRAISDGVFNAAIYDVVVHRDFQKQGIAKKIMEFLLDQLSHVSCVHLISTTGNEEFYRKLGLKRVKTGMARYLNPELSDEYLE',
-            'contig_1_10704_10537': 'MKGAESASFVLFLFHRSIRRAGNLHLKERANVPKECAKSGTIRKNFAHYFRIRLK',
-            'contig_1_11311_10706': 'MNPSLYHVVYFPLSTGGVMDFYRGLALGLGEEPKYRKVDLFRQIQQAIERLYHERRITPVFILDEMHLAKDAFLQDIAILFNFEMDSTNPFVLILAGLPHLQGKLRLNQHRPLDQRIIMRYRMGPLEKEEVAGYIKHRMKQAGAKHPIFTPSALEAIALQSRGWPRVINTLATTCLLYGYQLKKDAIDEEVVRMAAEEMGY',
-            'contig_1_11505_11365': 'MYKSFYSLSREPFAKETDPSEAYQGAPFQEALRALEYVKRTRGSGC',
-            'contig_1_13448_12804': 'MIQFHDFDIDVQTYAERGKENDFPLLKKCPHCRAKRPLHRHGYYERNALTPHGDYRIWIVRYRCRECLKTVSVLPSFLLPYFQYTLSAIWQVVKEQLGLTEGTNRAPFLPTKGRHHLLCPAVLPKPIKPSQLFCEAVEDHRPYREKRKGTGFLVDPDVGETRSLFGHQRHVGGRIPTPFCESNGILILHTYPKLEIVEVPTNLSYRRRKSDPVR'
-            }
-        self.assertTrue(queries == exp)
-        
-        
+    """        
     def test_prcs_blast_rslts_html(self):
         """
         Runs testing for the utility function prcs_blast_rslts_html()
